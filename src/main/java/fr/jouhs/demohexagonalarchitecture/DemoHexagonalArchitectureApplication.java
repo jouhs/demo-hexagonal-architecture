e9dfc5eff621c495bf561ebc53a74f6056722f01
jouhs <jaouad.ouhs@gmail.com>
2024-01-29 13:25:29 +0100

    add main packages of hexagoanl arch

---
+++ src/main/java/fr/jouhs/demohexagonalarchitecture/DemoHexagonalArchitectureApplication.java
@@ -0,0 +1,13 @@
+package fr.jouhs.demohexagonalarchitecture;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DemoHexagonalArchitectureApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoHexagonalArchitectureApplication.class, args);
+	}
+
+}
